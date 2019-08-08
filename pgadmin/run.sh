08e92fec0f565c18f5412799bf72347e3d3a21c7
Mike <mike@arsquanta.com>
2019-08-08 09:59:56 -0700

    pgadmin works

---
+++ pgadmin/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# standard way to run
+# that saves state between sessions
+# no need to use servers.json
+# -v ${HOME}/pgadmin/servers.json:/servers.json \
+# the /var/lib/pgadmin seems to save all state
+source .config
+
+docker run -p ${PORT}:80 \
+        --rm \
+        --name ${CONTAINER_NM} \
+        -v ${HOME}/pgadmin/lib:/var/lib/pgadmin \
+        -e PGADMIN_DEFAULT_EMAIL=${PGADMIN_DEFAULT_EMAIL} \
+        -e PGADMIN_DEFAULT_PASSWORD=$(security find-generic-password -a ${USER} -s pgadmin -w) \
+        -d dpage/pgadmin4
